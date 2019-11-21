72b4b5963626be2bf7ddb6e3872bebe39b874cbd
vladko <vladkozhuhov@gmail.com>
2019-11-21 10:12:57 -0800

    work,have to check pictures

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 #define _USE_MATH_DEFINES
-
+#include <ctype.h>
 #include "math.h"
 
-int width =4;// 4;// 1080;
-int hight = 3;//3;// 1920;
+int width =225;// 4;// 1080;
+int hight = 225;//3;// 1920;
 #define fromSlave 2
 #define fromMaster 1
 //char* fileName = "leafA.pgm";
-char* fileName = "/home/vladko/CLionProjects/Mpi_hw1/test1.pgm";
+char* fileName = "/home/vladko/CLionProjects/Mpi_hw1/leafA.pgm";
 
 void fillMatrix(int **matrix){
     FILE* file = fopen(fileName, "r");
@@ -24,40 +24,58 @@ void fillMatrix(int **matrix){
     int counterWidth = 0;
     int counterHight = 0;
     a = fgetc(file);
-    printf("Opened FIle and ready to fill matrix\n");
-    while (a != EOF )
+    int flag =1;
+    printf("Opened Ile and ready to fill matrix\n");
+    while (a != EOF & counterHight < hight)
     {
         if(counter < skip){
             if(a == newLine)
                 counter ++;
         }
         else{
-            if(a == newLine){
-                matrix[counterHight][counterWidth] = num;
+            if(counterWidth == width){//} && a == newLine){
+//                matrix[counterHight][counterWidth] = num;
+                printf("%d " ,counterHight);
+
                 counterHight++;
                 counterWidth = 0;
-                num = 0;
+//                flag =0;
+//                num = 0;
+                if( counterHight ==223){
+                    int jj=0;
+                }
+
             }
-            else if(a != space) {
+            if(isdigit(a)){//} != space ) {
                 num *= 10;
                 num += a - '0';
+                flag =1;
             }
             else{
-                matrix[counterHight][counterWidth] = num;
-                num = 0;
-                counterWidth ++;
+                if(flag == 1) {
+                    if( counterWidth ==223){
+                        int aaa= 1;
+                    }
+                    matrix[counterHight][counterWidth] = num;
+//                    printf("%d(%d) ", num, counterWidth);
+
+                    num = 0;
+                    flag =0;
+                    counterWidth++;
+                }
             }
         }
         a = fgetc(file);
     }
-
+    printf("FILLED");
 
     fclose(file);
 }
 void printMatrix(double matrix[ hight][width]){
     FILE* fileNew = fopen("/home/vladko/CLionProjects/Mpi_hw1/output.pgm", "w");
     fprintf(fileNew,"%s \n", "P2");
-    fprintf(fileNew,"%s \n", "4 3");//width [4] [3]
+
+    fprintf(fileNew,"%d %d \n", width,hight);//width [4] [3]
     fprintf(fileNew,"%s \n", "255");
     for(int  i = 0; i<  hight  ; i++){
         for(int j = 0; j< width; j++){
@@ -74,9 +92,9 @@ void printMatrix(double matrix[ hight][width]){
 int main(int argc, char * argv[]){
 
     //int matrix[width][hight];
-    int** matrixtmp = (int**)(malloc(sizeof(int*)* hight));
+    int** matrixtmp = (int**)(malloc(sizeof(int*)* (hight+1)));
     for(int i=0; i < hight; i++){
-        matrixtmp[i] = (int*)(malloc(sizeof(int)*width));
+        matrixtmp[i] = (int*)(malloc(sizeof(int)*(width+1)));
     }
 //    int** matrixNew = (int**)(malloc(sizeof(int*)* hight));
 //    for(int i=0; i < hight; i++){
@@ -86,7 +104,8 @@ int main(int argc, char * argv[]){
     double matrixNew[hight][width];
     double Res[hight][width];
 
-    //  printMatrix(matrix);
+
+//      printMatrix(matrix);
 
     int  numtasks,numworkers, rank, len, rc,rowNum,limit,extra,rows;
     char hostname[MPI_MAX_PROCESSOR_NAME];
@@ -109,35 +128,38 @@ int main(int argc, char * argv[]){
 /********Master Task*********/
     if( rank == 0 ) {
         fillMatrix(matrixtmp);
-        for(int i =0;i < hight;++i){
-            for(int j = 0; j < width;++j ) {
-               matrix[i][j] = matrixtmp[i][j];
-                printf("%d ",  matrix[i][j]);
+        for (int i = 0; i < hight; ++i) {
+            for (int j = 0; j < width; ++j) {
+                matrix[i][j] = matrixtmp[i][j];
+                if (matrix[i][j] == 0)
+                    printf(" [i = %d, j = %d]  \n", i, j);
 
             }
-            printf("\n");
-            }
-        printf("CHECK %d\n",matrix[0][3]);
+
+        }
+
+        printf("CHECK %d\n", matrix[0][3]);
         rowNum = hight / numworkers;
-        printf("rowNum %d\n", rowNum);
+//        printf("rowNum %d
+//        \n", rowNum);
         extra = hight % numworkers;
         /* send tasks to workers*/
         for (int dest = 1; dest <= numworkers; ++dest) {
             if (dest != numworkers) {
                 rows = (dest - 1) * rowNum;
-                limit = rows + rowNum ;
+                limit = rows + rowNum;
 
             } else {
                 rows = (dest - 1) * rowNum;
                 limit = rows + rowNum + extra;
             }
-            MPI_Send(&rows, 1, MPI_INT, dest, fromMaster+1,  MPI_COMM_WORLD);
+            MPI_Send(&rows, 1, MPI_INT, dest, fromMaster + 1, MPI_COMM_WORLD);
 //            printf("Send from source to worker%d -  row num:%d\n",dest,rows);
-            MPI_Send(&limit, 1, MPI_INT, dest, fromMaster+2,  MPI_COMM_WORLD);
+            MPI_Send(&limit, 1, MPI_INT, dest, fromMaster + 2, MPI_COMM_WORLD);
 //            printf("Send from source to worker%d -  limit num:%d\n",dest,limit);
-            MPI_Send(&matrix, hight*width , MPI_INT, dest, fromMaster+3, MPI_COMM_WORLD);
+            MPI_Send(&matrix, hight * width, MPI_INT, dest, fromMaster + 3, MPI_COMM_WORLD);
 //            printf("Send from source to worker%d - original matrix\n",dest);
-            MPI_Send(&matrixNew, hight*width, MPI_DOUBLE, dest, fromMaster+4, MPI_COMM_WORLD);
+            MPI_Send(&matrixNew, hight * width, MPI_DOUBLE, dest, fromMaster + 4, MPI_COMM_WORLD);
 //            printf("Send from source to worker%d - new matrix\n",dest);
 
         }
@@ -145,22 +167,22 @@ int main(int argc, char * argv[]){
         /* Receive results from workers*/
         for (int i = 1; i <= numworkers; i++) {
             int source = i;
-            MPI_Recv(&rows, 1, MPI_INT, source, fromSlave+1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&rows, 1, MPI_INT, source, fromSlave + 1, MPI_COMM_WORLD, &status);
 //            printf("Recv from worker%d to source -  row number:%d\n",source, rows);
-            MPI_Recv(&limit, 1, MPI_INT, source, fromSlave+2, MPI_COMM_WORLD, &status);
+            MPI_Recv(&limit, 1, MPI_INT, source, fromSlave + 2, MPI_COMM_WORLD, &status);
 //            printf("Recv from worker%d to source -  limit number:%d\n",source, limit);
-            MPI_Recv(matrix, hight*width , MPI_INT, source, fromSlave+3, MPI_COMM_WORLD, &status);
+            MPI_Recv(matrix, hight * width, MPI_INT, source, fromSlave + 3, MPI_COMM_WORLD, &status);
 //            printf("Recv from worker%d to source -  original matrix\n",source);
-            MPI_Recv(&matrixNew, hight*width, MPI_DOUBLE, source, fromSlave+4, MPI_COMM_WORLD, &status);
+            MPI_Recv(&matrixNew, hight * width, MPI_DOUBLE, source, fromSlave + 4, MPI_COMM_WORLD, &status);
 //            printf("Recv from worker%d to source -  new matrix\n",source);
-            for(int ii =i-1 ;ii < limit;++ii){
-                for(int j = 0; j < width;++j ) {
+            for (int ii = i - 1; ii < limit; ++ii) {
+                for (int j = 0; j < width; ++j) {
                     Res[ii][j] = matrixNew[ii][j];
-                    printf("Enter value   %lf from wroker %d\n",matrixNew[ii][j],i);
+//                    printf("Enter value   %lf from wroker %d\n",matrixNew[ii][j],i);
 //                    printf("Enter matrix  VAL  %d from wroker %d index %d\n",matrix[0][0],i,ii);
                 }
             }
-            printf("Limit %d from worker %d \n",limit,i);
+//            printf("Limit %d from worker %d \n",limit,i);
 
         }
 
@@ -190,31 +212,42 @@ int main(int argc, char * argv[]){
 
 //        matrix[1][1] = -1;
 //        matrix[0][0] = -2;
-    printf("HEEEREEEE");
-    printf("HEEEREEEE\n");
-        int ii = rank-1;
-        printf("%d iiii\n",ii);
+//    printf("HEEEREEEE");
+//    printf("HEEEREEEE\n");
+//        int ii = rank-1;
+//        printf("%d iiii\n",ii);
 
-        for(;ii < limit;++ii){
-            for(int j = 0; j < width;++j ){
-                if(ii != 0 && ii != hight-1 && j !=0  && j!= width-1) {
-
-                    printf("%d %d", matrix[ii][j], matrix[ii - 1][j]);
-                }
-            }
-            printf("\n");
-        }
+//        for(;ii < limit;++ii){
+//            for(int j = 0; j < width;++j ){
+//                if(ii != 0 && ii != hight-1 && j !=0  && j!= width-1) {
+//
+//                    printf("%d %d", matrix[ii][j], matrix[ii - 1][j]);
+//                }
+//            }
+//            printf("\n");
+//        }
 
         int i = rank-1;
         for(;i < limit;++i){
             for(int j = 0; j < width;++j ){
                 if(i != 0 && i != hight-1 && j !=0  && j!= width-1){
                     double y = 1.0/9.0;
-                    printf("%d %d %d %d %d %d %d %d %d",matrix[i-1][j]*matrix[i-1][j-1]*matrix[i][j]*matrix[i][j+1]* matrix[i+1][j+1]*matrix[i+1][j]*matrix[i][j-1]*matrix[i+1][j-1]*matrix[i-1][j+1]);
-                    double x = matrix[i-1][j]*matrix[i-1][j-1]*matrix[i][j]*matrix[i][j+1]* matrix[i+1][j+1]*matrix[i+1][j]*matrix[i][j-1]*matrix[i+1][j-1]*matrix[i-1][j+1];
-                    printf("x %lf and y %lf\n",x,y);
+//                    printf("values from matrix: ");
+//                    printf("matrix[%d][%d] = %d \n",i-1,j,matrix[i-1][j]);
+//                    printf("matrix[%d][%d] = %d \n",i-1,j-1,matrix[i-1][j-1]);
+//                    printf("! matrix[%d][%d] = %d \n",i,j,matrix[i][j]);
+//                    printf("matrix[%d][%d] = %d \n",i,j+1,matrix[i][j+1]);
+//                    printf("matrix[%d][%d] = %d \n",i+1,j+1,matrix[i+1][j+1]);
+//                    printf("matrix[%d][%d] = %d \n",i+1,j,matrix[i+1][j]);
+//                    printf("matrix[%d][%d] = %d \n",i,j-1,matrix[i][j-1]);
+//                    printf("matrix[%d][%d] = %d \n",i+1,j-1,matrix[i+1][j-1]);
+//                    printf("matrix[%d][%d] = %d \n",i-1,j+1,matrix[i-1][j+1]);
+
+//                    printf("%d %d %d %d %d %d %d %d %d",matrix[i-1][j],matrix[i-1][j-1],matrix[i][j],matrix[i][j+1], matrix[i+1][j+1],matrix[i+1][j],matrix[i][j-1],matrix[i+1][j-1],matrix[i-1][j+1]);
+                    double x = 1.0*matrix[i-1][j]*matrix[i-1][j-1]*matrix[i][j]*matrix[i][j+1]* matrix[i+1][j+1]*matrix[i+1][j]*matrix[i][j-1]*matrix[i+1][j-1]*matrix[i-1][j+1];
+//                    printf("x %lf and y %lf\n",x,y);
                     matrixNew[i][j]=pow(x,y);
-                    printf("NEW VALUE %lf , old value %d\n",matrixNew[i][j], matrix[i][j]);
+//                    printf("NEW VALUE %lf , old value %d\n",matrixNew[i][j], matrix[i][j]);
                 }
                 else{
                     matrixNew[i][j]=matrix[i][j];
@@ -232,10 +265,11 @@ int main(int argc, char * argv[]){
         MPI_Send(&matrixNew, hight*width, MPI_DOUBLE, 0, fromSlave+4, MPI_COMM_WORLD);
 //        printf("Send from worker%d to source -  new matrix \n",rank);
     }
+
     MPI_Finalize();
 
 
-//
+
 //    for (int j = 0; j < hight; j++) {
 //        free(matrix[j]);
 //    }
